012ee84a28dec83aceb70389b0bdd8eb5181f5a7
Moritz Wilhelm <moritz.wilhelm@cispa.de>
2023-05-25 23:01:51 +0200

    fix setup.py, extend .gitignore, update requirements

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='master-thesis', version='1.0', packages=find_packages())
+setup(
+    name='master-thesis',
+    version='1.0',
+    packages=find_packages(),
+    package_data={'configs': ['tranco_W9JG9.csv']},
+    include_package_data=True
+)
