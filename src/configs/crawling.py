be905bd237a38223993dab8ea6ee006529db7ad3
Moritz Wilhelm <moritz.wilhelm@cispa.de>
2023-06-17 19:21:05 +0200

    rename date to timestamp

--- src/configs/crawling.py
+++ src/configs/crawling.py
@@ -1,4 +1,5 @@
 PREFIX = 'http://www.'
 USER_AGENT = \
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
-INTERNET_ARCHIVE_URL = 'https://web.archive.org/web/{date}/{url}'
+INTERNET_ARCHIVE_URL = 'https://web.archive.org/web/{timestamp}/{url}'
+INTERNET_ARCHIVE_TIMESTAMP_FORMAT = '%Y%m%d%H%M%S'
--- src/data_collection/collect_archive_data.py
+++ src/data_collection/collect_archive_data.py
@@ -8,14 +8,14 @@ from typing import NamedTuple, List, Dict, Optional
 
 import requests
 
-from configs.crawling import PREFIX, INTERNET_ARCHIVE_URL
+from configs.crawling import PREFIX, INTERNET_ARCHIVE_URL, INTERNET_ARCHIVE_TIMESTAMP_FORMAT
 from configs.database import get_database_cursor
 from configs.utils import get_absolute_tranco_file_path
 from data_collection.crawling import setup, reset_failed_crawls, partition_jobs, crawl, CrawlingException
 
 WORKERS = 8
 
-DATES = [
+TIMESTAMPS = [
     datetime(2016, 1, 15, 12), datetime(2016, 4, 15, 12), datetime(2016, 7, 15, 12), datetime(2016, 10, 15, 12),
     datetime(2017, 1, 15, 12), datetime(2017, 4, 15, 12), datetime(2017, 7, 15, 12), datetime(2017, 10, 15, 12),
     datetime(2018, 1, 15, 12), datetime(2018, 4, 15, 12), datetime(2018, 7, 15, 12), datetime(2018, 10, 15, 12),
@@ -26,12 +26,12 @@ DATES = [
     datetime(2023, 1, 15, 12), datetime(2023, 4, 15, 12),  # datetime(2023, 7, 15, 12), datetime(2023, 10, 15, 12),
 ]
 
-TABLE_NAME = "archive_data_{date}"
+TABLE_NAME = "archive_data_{timestamp}"
 
 
 class ArchiveJob(NamedTuple):
     """Represents a job for crawling the archive and storing data in the database."""
-    date: str
+    timestamp: str
     tranco_id: int
     domain: str
     url: str
@@ -42,37 +42,37 @@ def worker(jobs: List[ArchiveJob]) -> None:
     """Crawl all provided `urls` and store the responses in the database."""
     with get_database_cursor(autocommit=True) as cursor:
         session = requests.Session()
-        for date, tranco_id, domain, url, proxies in jobs:
+        for timestamp, tranco_id, domain, url, proxies in jobs:
             time.sleep(0.2)
             try:
                 response = crawl(url, proxies=proxies, session=session)
                 cursor.execute(f"""
-                    INSERT INTO {TABLE_NAME.format(date=date)} 
+                    INSERT INTO {TABLE_NAME.format(timestamp=timestamp)} 
                     (tranco_id, domain, start_url, end_url, status_code, headers, content_hash, response_time) 
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                 """, (tranco_id, domain, url, *response.serialized_data))
             except CrawlingException as error:
                 cursor.execute(f"""
-                    INSERT INTO {TABLE_NAME.format(date=date)} 
+                    INSERT INTO {TABLE_NAME.format(timestamp=timestamp)} 
                     (tranco_id, domain, start_url, headers) 
                     VALUES (%s, %s, %s, %s)
                 """, (tranco_id, domain, url, error.to_json()))
 
 
-def prepare_jobs(tranco_file: Path, dates: List[str], n: int = 20000) -> List[ArchiveJob]:
-    """Build a list of ArchiveJob instances for the given Tranco file, dates, and maximum number of domains per date."""
+def prepare_jobs(tranco_file: Path, timestamps: List[str], n: int = 20000) -> List[ArchiveJob]:
+    """Generate ArchiveJob list for Tranco file, timestamps, and max domains per timestamp."""
     worked_urls = defaultdict(set)
-    for date in dates:
-        worked_urls[date] = reset_failed_crawls(TABLE_NAME.format(date=date))
+    for timestamp in timestamps:
+        worked_urls[timestamp] = reset_failed_crawls(TABLE_NAME.format(timestamp=timestamp))
 
     jobs = []
     with open(tranco_file) as file:
         for line in islice(file, n):
             tranco_id, domain = line.strip().split(',')
-            for date in dates:
-                url = INTERNET_ARCHIVE_URL.format(date=date, url=f"{PREFIX}{domain}")
-                if url not in worked_urls[date]:
-                    jobs.append(ArchiveJob(date, tranco_id, domain, url, None))
+            for timestamp in timestamps:
+                url = INTERNET_ARCHIVE_URL.format(timestamp=timestamp, url=f"{PREFIX}{domain}")
+                if url not in worked_urls[timestamp]:
+                    jobs.append(ArchiveJob(timestamp, tranco_id, domain, url, None))
 
     return jobs
 
@@ -84,12 +84,12 @@ def run_jobs(jobs: List[ArchiveJob]) -> None:
 
 
 def main():
-    dates = [date.strftime('%Y%m%d%H%M%S') for date in DATES]
-    for date in dates:
-        setup(TABLE_NAME.format(date=date))
+    timestamps = [timestamp.strftime(INTERNET_ARCHIVE_TIMESTAMP_FORMAT) for timestamp in TIMESTAMPS]
+    for timestamp in timestamps:
+        setup(TABLE_NAME.format(timestamp=timestamp))
 
     # Prepare and execute the crawl jobs
-    jobs = prepare_jobs(get_absolute_tranco_file_path(), dates)
+    jobs = prepare_jobs(get_absolute_tranco_file_path(), timestamps)
     run_jobs(jobs)
 
 
