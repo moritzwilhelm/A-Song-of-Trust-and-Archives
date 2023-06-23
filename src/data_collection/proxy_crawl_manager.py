a04d122434c98499219a49664ea310e6a04301ef
Moritz Wilhelm <moritz.wilhelm@cispa.de>
2023-06-23 19:24:01 +0200

    add crawl manager that distributes crawls via proxies

---
+++ src/data_collection/proxy_crawl_manager.py
@@ -0,0 +1,44 @@
+from argparse import ArgumentParser, Namespace as Arguments
+from multiprocessing import Pool
+from typing import List, Set, Dict, Optional
+
+from configs.crawling import TIMESTAMPS
+from data_collection.collect_archive_proximity_set import crawl_web_archive_cdx
+from data_collection.crawling import partition_jobs
+
+
+def build_proxies(ports: List[int]) -> Set[Optional[Dict[str, str]]]:
+    """Create the set of proxies based on the provided `ports`."""
+    return {dict(http=f"socks5://localhost:{port}", https=f"socks5://localhost:{port}") for port in ports} | {None}
+
+
+def parse_args() -> Arguments:
+    """Parse command line arguments for starting crawlers distributed over different proxies."""
+    parser = ArgumentParser(description='Start archive crawlers and distribute over .')
+    parser.add_argument('crawl_type', metavar='<crawl_type>', choices=['proximity_set_indexes'],
+                        help='the type of crawl to start')
+    parser.add_argument('-p', '--ports', metavar='<port>', type=int, nargs='*', default=set(),
+                        help='an open socks proxy port')
+    return parser.parse_args()
+
+
+def start_collect_archive_proximity_set_indexes(proxies: Set[Optional[Dict[str, str]]]) -> None:
+    """Start crawling the Internet Archive CDX server with the provided set of proxies."""
+
+    def crawl_web_archive_cdx_worker(timestamps, proxy):
+        return crawl_web_archive_cdx(timestamps=timestamps, proxies=proxy)
+
+    with Pool(len(proxies)) as pool:
+        pool.starmap(crawl_web_archive_cdx_worker, zip(partition_jobs(TIMESTAMPS, len(proxies)), proxies))
+
+
+def main():
+    args = parse_args()
+    proxies = build_proxies(args.ports)
+
+    if args.crawl_type == 'proximity_set_indexes':
+        start_collect_archive_proximity_set_indexes(proxies)
+
+
+if __name__ == '__main__':
+    main()
