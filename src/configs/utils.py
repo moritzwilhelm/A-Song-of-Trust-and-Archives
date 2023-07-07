79c1ea6b37971e9111949ab5896309a548fdc28b
Moritz Wilhelm <moritz.wilhelm@cispa.de>
2023-07-08 00:33:58 +0200

    add PROJECT_ROOT constant (dynamically set during setup)

--- src/configs/utils.py
+++ src/configs/utils.py
@@ -4,20 +4,23 @@ from typing import List, Tuple
 
 from configs.crawling import NUMBER_URLS, PREFIX
 
+PROJECT_ROOT = Path('<AUTOMATICALLY-REPLACED-DURING-INSTALL>')
+INSTALLED_PROJECT_ROOT = Path(__file__).parents[1].resolve()
+
 
 def join_with_json_path(filename: str) -> Path:
     """Return absolute path to the specified `filename` in the results/json directory."""
-    return Path(__file__).parents[1].resolve().joinpath('results', 'json', filename)
+    return PROJECT_ROOT.joinpath('results', 'json', filename)
 
 
 def join_with_plots_path(filename: str) -> Path:
     """Return absolute path to the specified `filename` in the results/json directory."""
-    return Path(__file__).parents[1].resolve().joinpath('results', 'plots', filename)
+    return PROJECT_ROOT.joinpath('results', 'plots', filename)
 
 
 def get_absolute_tranco_file_path() -> Path:
     """Return absolute path to the Tranco file."""
-    return Path(__file__).parent.resolve().joinpath('tranco_W9JG9.csv')
+    return PROJECT_ROOT.joinpath('configs', 'tranco_W9JG9.csv')
 
 
 def get_tranco_data(tranco_file: Path = get_absolute_tranco_file_path(),
--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,21 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
+
+
+def update_utils_file() -> None:
+    """Dynamically update PROJECT_SOURCE constant."""
+    utils_file_path = Path(__file__).parent.resolve().joinpath('configs', 'utils.py')
+
+    with open(utils_file_path) as file:
+        utils_file_content = file.read()
+
+    with open(utils_file_path, 'w') as file:
+        file.write(utils_file_content.replace('<AUTOMATICALLY-REPLACED-DURING-INSTALL>',
+                                              str(Path(__file__).parent.resolve())))
+
+
+update_utils_file()
 
 setup(
     name='master-thesis',
