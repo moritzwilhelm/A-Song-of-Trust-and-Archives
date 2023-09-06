45e1dc5f40fe001f84be0446c70899365de19591
Moritz Wilhelm <moritz.wilhelm@cispa.de>
2023-09-07 00:07:27 +0200

    fix time of TODAY timestamp

--- src/configs/crawling.py
+++ src/configs/crawling.py
@@ -13,7 +13,7 @@ USER_AGENT = \
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 
 # TIMESTAMP CONSTANTS
-TODAY = datetime.now(UTC)
+TODAY = datetime.now(UTC).replace(hour=12, minute=0, second=0, microsecond=0)
 FINAL_TIMESTAMP = datetime(2023, 4, 15, 12, tzinfo=UTC)
 TIMESTAMPS = tuple(
     timestamp
