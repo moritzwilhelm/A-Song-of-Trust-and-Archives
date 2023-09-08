7bf82d71c016ee7ce9f934bc4a4cf23cfcaf97e2
Moritz Wilhelm <moritz.wilhelm@cispa.de>
2023-09-08 17:48:36 +0200

    add security mechanism -> header mapping

--- src/configs/analysis.py
+++ src/configs/analysis.py
@@ -9,6 +9,20 @@ RELEVANT_HEADERS = (
     'Cross-Origin-Embedder-Policy'
 )
 
+SECURITY_MECHANISM_HEADERS = {
+    'X-Frame-Options': 'X-Frame-Options',
+    'Content-Security-Policy::XSS': 'Content-Security-Policy',
+    'Content-Security-Policy::FA': 'Content-Security-Policy',
+    'Content-Security-Policy::TLS': 'Content-Security-Policy',
+    'Content-Security-Policy': 'Content-Security-Policy',
+    'Strict-Transport-Security': 'Strict-Transport-Security',
+    'Referrer-Policy': 'Referrer-Policy',
+    'Permissions-Policy': 'Permissions-Policy',
+    'Cross-Origin-Opener-Policy': 'Cross-Origin-Opener-Policy',
+    'Cross-Origin-Resource-Policy': 'Cross-Origin-Resource-Policy',
+    'Cross-Origin-Embedder-Policy': 'Cross-Origin-Embedder-Policy'
+}
+
 INTERNET_ARCHIVE_SOURCE_HEADER = 'X-Archive-Src'
 INTERNET_ARCHIVE_HEADER_PREFIX = 'X-Archive-Orig-'
 INTERNET_ARCHIVE_END_URL_REGEX = r'^(?:[^\/]*\/){5}(.*)'
