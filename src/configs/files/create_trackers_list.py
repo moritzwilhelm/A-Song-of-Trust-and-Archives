4cbc5bbaed9e4fcbf6c959e742e4b9007e043c6a
Moritz Wilhelm <moritz.wilhelm@cispa.de>
2023-09-14 00:30:14 +0200

    add easyprivacy

--- src/configs/files/create_trackers_list.py
+++ src/configs/files/create_trackers_list.py
@@ -1,4 +1,5 @@
 import json
+import re
 from itertools import chain
 
 from configs.utils import PROJECT_ROOT
@@ -6,13 +7,20 @@ from configs.utils import PROJECT_ROOT
 
 def create_trackers_json() -> None:
     """Parse Disconnect list and create tracking.json containing the set of tracking domains."""
-    with open(PROJECT_ROOT.joinpath('src', 'configs', 'disconnect_entities.json')) as file:
+    with open(PROJECT_ROOT.joinpath('src', 'configs', 'files', 'disconnect_entities.json')) as file:
         entities = json.load(file)
 
     trackers = set()
     for entity_name, entity_info in entities['entities'].items():
         for domain in chain(*entity_info.values()):
             trackers.add(domain)
+
+    with open(PROJECT_ROOT.joinpath('src', 'configs', 'files', 'easyprivacy.txt')) as file:
+        for line in file:
+            if line.startswith('||'):
+                trackers.add(re.match(r'\|\|([^/^$]*)([/^$].*)?', line).group(1))
+            elif line.startswith('|'):
+                trackers.add(re.match(r'\|https?://([^/^$]*)([/^$].*)?', line).group(1))
 
     with open(PROJECT_ROOT.joinpath('src', 'configs', 'files', 'trackers.json'), 'w') as file:
         json.dump(sorted(trackers), file, indent=2)
